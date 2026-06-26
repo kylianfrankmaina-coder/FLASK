2dcba11c57b91545bebc17cc8639226f5a49ace1
kylianfrankmaina-coder <kylianfrankmaina@gmail.com>
2026-06-26 13:25:12 +0300

    add signing

--- app.py
+++ app.py
@@ -16,12 +16,78 @@ def signup():
 
     # connection to database 
     connection= pymysql.connect(
-        host="root",
-        user="localhost",
+        host="localhost",
+        user="root",
         password="",
         database="modcomfrancis",
 
     )
+    # define the cursor
+    cursor=connection.cursor()
+    
+    # define sql to insert users
+    sql="insert into users(username,password,email,phone) values(%s,%s,%s,%s)"
+    # define your data
+    # NB:coming from step 3
+    data=(username,password,email,phone)
+    # execute/run query
+    cursor.execute(sql,data)
+    # commtit/save changes
+    connection.commit()
+    return jsonify({"message":"user registration successfully"})
+
+
+# member signin/login
+ # define route/endpoint
+@app.route("/api/signin",methods=["POST"])
+# define the function
+def signin():
+    # get user inputs from the form
+    email= request.form["email"]
+    password=request.form["password"]
+
+    # connection to database
+    connection= pymysql.connect(
+        host="localhost",
+        user="root",
+        password="",
+        database="modcomfrancis",
+    )
+    # define the cursor
+    cursor=connection.cursor(pymysql.cursors.DictCursor)
+    # define sql to select users
+    sql="select * from users where email =%s and password=%s"
+    # define your data
+    # NB: its coming from step 3
+    data = (email , password)
+    # execute/run query
+    cursor.execute(sql,data)
+    # wrong email and password
+    if cursor.rowcount==0 :
+        return jsonify({"message":"invalid email or password"
+        })
+    # correct email and password
+    if cursor.rowcount==1 :
+        # fetch the user,
+        user = cursor.fetchone()
+        return jsonify ({"message":"login successful",
+                         "user":user})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 # run the application
